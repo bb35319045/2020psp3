b4ec6f6217085d4fc6f3d42e1cbed3086325da0b
bb35319045 <bb35319045@ms.nagasaki-u.ac.jp>
2020-10-01 10:02:32 +0900

    テスト

--- k00/k00.c
+++ k00/k00.c
@@ -2,5 +2,11 @@
 
 int main(void)
 {
-    printf("Hello World\n");
+    int n,i;
+    printf("number:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+    printf("Hello World!\n");
+    }
+    return 0;
 }
