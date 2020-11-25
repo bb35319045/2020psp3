a31e9ee4db5d9954bfbaa013ec91de48c9f3020e
bb35319045 <bb35319045@ms.nagasaki-u.ac.jp>
2020-11-25 17:13:29 +0900

    k03 初回提出

--- k03/README.md
+++ k03/README.md
@@ -1,5 +1,5 @@
 # 課題3 レポート
-学生番号　氏名
+学生番号 35319045　氏名 松本聡浩
 
 
 ## 課題  
@@ -31,18 +31,35 @@
     [出力]  
     - return値：検索する文字列が出現した場所(ポインタ)。ただし、検索する文字列が見つからない場合はNULL。  
 
-3. 補助関数  
+1. 補助関数  
 なし
 
 ## ソースコードの説明
+ForceSearch:
+12~15:ForceSearch関数で使う、pos、start、Original_len、Key_lenを0で初期化。
+17~19:Original_lenをStrOriginalの文字列の長さに更新。
+21~23:Key_lenをStrKeyの文字列の長さに更新。
+26,27:keyとtextが一致したらposを1つ進める。
+28:keyが'\0'なら&text[start]を返し、ループを抜ける。
+32~34:keyの長さ(Key_len)と比較開始位置(start)の和がtext(Original_len)の長さを超えたら、NULLを返しループを抜ける。
+35~37:それら以外の時、startを1つ進め、pos=0に初期化。
 
-
+BMSearch:
+45~51:BMSearch関数で使う、Original_index、Key_index、Original_len、Key_lenを0de初期化し、table、for文で使うi、uを定義。
+54~56:Original_lenをStrOriginalの文字列の長さに更新。
+58~60:Key_lenをStrKeyの文字列の長さに更新。
+62~64:tableのすべての要素をkey_lenの値にする。
+66~68:keyに含まれる文字をそれぞれ位置によって定義しなおす。
+70、71:Original_index、Key_indexを文字列の一番後ろに移動。
+75~82:文字が一致した場合の条件
+76~78::Key_indexが一番最初に移動していたら、&text[Original_index]を返し、ループを抜ける。
+79~82:それ以外ならKey_index、Original_indexを1つ戻す。
+83~91:文字が不一致の場合の条件
+84~86:Original_indexが文字列の長さを超えたら、NULLを返し、ループを抜ける。
+88、89:Original_index、Key_indexの位置を更新
 
 ## 出力結果
-
-```
-
-```
-
+Force Search. Find keyword at: wind in my hair.
+BM Search. Find keyword at: wind in my hair.
 ## 修正履歴
 
--- k03/k03.c
+++ k03/k03.c
@@ -9,24 +9,98 @@ char StrKey[] = "wind";
 
 char* ForceSearch(char text[], char key[])
 {
-    //  ここを実装する
+  int pos = 0;
+  int start = 0;
+  int Original_len = 0;
+  int Key_len = 0;
 
+  while(StrOriginal[Original_len] != '\0'){
+      Original_len++;
+    }
+  
+  while(StrKey[Key_len] != '\0'){
+      Key_len++;
+    }
+
+  while(1){
+  if(key[pos] == text[start+pos]){
+      pos++;
+     }else if(key[pos] == '\0'){
+       return &text[start];
+       break;
+     }else{
+       if((Key_len + start) > Original_len){
+          return NULL;
+          break;
+     }else{
+      start++;
+      pos = 0;
+    }
+  }
+ }
 }
 
 char* BMSearch(char text[], char key[])
-{
-    //  ここを実装する
+{ 
+  int Original_index = 0;
+  int Key_index = 0;
+  int table[ALPHABET_LEN];
+  int Original_len = 0;
+  int Key_len = 0;
+  int i;
+  int u;
 
+  
+ while(StrOriginal[Original_len] != '\0'){
+      Original_len++;
+    }
+  
+ while(StrKey[Key_len] != '\0'){
+      Key_len++;
+    }
+
+  for(i = 0; i <= ALPHABET_LEN; i++){
+    table [i] = Key_len;
+  }
+
+  for(u = 0; u < Key_len; u++){
+    table [key[u]] =  Key_len - (u + 1);
+  }
+
+  Original_index = Key_len - 1;
+  Key_index = Key_len - 1;
+  
+
+  while(1){
+  if(key[Key_index] == text[Original_index]){
+     if(Key_index == 0){
+        return &text[Original_index];
+        break;
+     }else{
+    Key_index--;
+    Original_index--;
+     }
+  }else{
+     if(Original_index > Original_len){
+        return NULL;
+        break;
+     }else{
+        Original_index = Original_index + table[text[Original_index]];
+        Key_index = Key_len - 1;
+   }
+  }
+ }
 }
+
 
 int main(void)
 {
     char*   str;
     str = ForceSearch(StrOriginal, StrKey);
-    printf("Force Search. Find keyword at:%s\n", str);
+    printf("Force Search. Find keyword at: %s\n", str);
 
     str = BMSearch(StrOriginal, StrKey);
-    printf("BM Search. Find keyword at:%s\n", str);
+    printf("BM Search. Find keyword at: %s\n", str);
 
     return 0;
 }
