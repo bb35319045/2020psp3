abf2f51f5ad5f9282bfcf0824bd43dcaca7a776a
bb35319045 <bb35319045@ms.nagasaki-u.ac.jp>
2021-01-07 20:56:40 +0900

    k04 2回目提出

--- k04/k04.c
+++ k04/k04.c
@@ -85,19 +85,16 @@ void BubbleSort(City arrayCity[], int size)
    while(1){
        cnt = 0;
        for(pos=0; pos<size-1; pos++){
-         if(arrayCity[pos].total < arrayCity[pos+1].total){  
+         if(arrayCity[pos].total > arrayCity[pos+1].total){  
             array_tmp = arrayCity[pos];
             arrayCity[pos] = arrayCity[pos+1];
             arrayCity[pos+1] = array_tmp;
             cnt++;
-        }else{   
+         }  
        }
-   }
        if(cnt == 0){
            break;
        }
-       else{       
-       } 
    }
 }
 
@@ -109,18 +106,18 @@ void QuickSort(City arrayCity[], int left, int right)
 
     int index_left;
     int index_right;
-    int size = right  - left + 1;
+    int size = right - left;
 
     
-    if(size > 1){
-       pivot = arrayCity[left];   
-       
+    if(size > 0){
+       pivot = arrayCity[left];  
+            
        while(1){
-         index_left = left + 1;
-         index_right = right;    
-         
+         index_left = left;
+         index_right = right;   
+                
          while(pivot.seafood >= arrayCity[index_left].seafood){
-            index_left++;          
+            index_left++;       
          }
          while(pivot.seafood < arrayCity[index_right].seafood){
             index_right--;
@@ -134,8 +131,8 @@ void QuickSort(City arrayCity[], int left, int right)
          }
        }
       
-       array_tmp = pivot;
-       pivot = arrayCity[index_right];
+       array_tmp = arrayCity[left];
+       arrayCity[left] = arrayCity[index_right];
        arrayCity[index_right] = array_tmp;
 
        QuickSort(arrayCity, left, index_right-1);
@@ -144,20 +141,101 @@ void QuickSort(City arrayCity[], int left, int right)
 }
 
 
+void ParticalHeap(City arrayCity[], int size, int node){
+    int left;
+    int right;
+    City array_tmp;
 
-void HeapSort(City arrayCity[], int size)
-{
-    //  チャレンジ問題(1)
-    //  ここを実装する
+    left = node * 2 + 1;
+    right = node * 2 + 2;
 
+    if((left > size && right > size) || (arrayCity[node].meat < arrayCity[left].meat && arrayCity[node].meat < arrayCity[right].meat)){  
+    
+    }else if(left < size && right < size){
+        if(arrayCity[left].meat  <= arrayCity[right].meat){
+         array_tmp = arrayCity[node];
+         arrayCity[node] = arrayCity[left];
+         arrayCity[left] = array_tmp; 
+         ParticalHeap(arrayCity, size, left);
+        }else{
+         array_tmp = arrayCity[node];
+         arrayCity[node] = arrayCity[right];
+         arrayCity[right] = array_tmp; 
+         ParticalHeap(arrayCity, size, right); 
+        }
+    }else{
+       if(arrayCity[left].meat  > arrayCity[node].meat){
+         array_tmp = arrayCity[node];
+         arrayCity[node] = arrayCity[left];
+         arrayCity[left] = array_tmp; 
+         ParticalHeap(arrayCity, size, left); 
+       }
+    }
 }
 
+void BuildHeap(City arrayCity[], int size){
+ 
+ int last_node;
+ int i;
 
-void MergeSort(City arrayCity[], int left, int right)
-{
-    //  チャレンジ問題2
-    //  ここを実装する
+ last_node = size /2 - 1;
 
+ for(i=last_node; i>=0; i--){
+    ParticalHeap(arrayCity, size, last_node);   
+ }
+}
+
+void HeapSort(City arrayCity[], int size){
+     int size_Heap;
+     City array_tmp;
+     BuildHeap(arrayCity, size);
+     
+     for(size_Heap=size; size_Heap>1; size_Heap--){
+      array_tmp = arrayCity[0];
+      arrayCity[0] = arrayCity[size-1];
+      arrayCity[size-1] = array_tmp;
+
+      BuildHeap(arrayCity, size-1);
+    }
+}
+
+void Merge(City arrayCity[], int left, int mid, int right){
+    int i,j,k;
+    City left_buff[mid-left+1];
+    City right_buff[right-mid];
+
+    
+    for (i = left; i <= mid; i++){ 
+     left_buff[i] = arrayCity[i]; 
+    } 
+    for (j = mid+1; j <= right; j++){
+      right_buff[j] = arrayCity[j]; 
+    }
+    i = left; j = mid+1;
+    
+    for (k = left; k <= right; k++){
+      if (left_buff[i].liquor < right_buff[j].liquor){ 
+        arrayCity[k] = left_buff[i]; 
+        i++;
+    }else{ 
+    arrayCity[k] = right_buff[j];
+    j++; 
+    }
+   }
+  }
+
+
+void MergeSort(City arrayCity[], int left, int right){
+    int mid;
+   
+     if(right > left){
+      mid = left+  (right - left) /2;
+     
+      MergeSort(arrayCity, left, mid);
+      MergeSort(arrayCity, mid+1, right);
+
+      Merge(arrayCity, left, mid, right);
+   }
 }
 
 int main(void)
